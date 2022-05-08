a464b7706ddc6ba1fd78caf65f7255e8b95f2377
Neil Allwood <neilallwood@icloud.com>
2022-05-08 16:43:39 +0100

    Updated app.py to create app outside the if.

--- app.py
+++ app.py
@@ -4,7 +4,8 @@
 # rather than the individual file names.
 from website import create_app, create_database
 
+app = create_app()
+
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug = True,
             host = "192.168.0.11")
